f1bbe37b2ecc8719e51189351121c49e9debe402
raphael-corize <raphael.hebert@corize.co.jp>
2024-05-05 11:54:06 +0200

    added dummy users

--- main.go
+++ main.go
@@ -14,13 +14,24 @@ func dices(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusOK).JSON(res)
 }
 
+func getUser(ctx *fiber.Ctx) error {
+	uid := ctx.Query("id")
+	// TODO connect to db and drop dummy data
+	res := fmt.Sprintf("%s", UsersList[uid])
+	return ctx.Status(fiber.StatusOK).JSON(res)
+}
+
 func main(){
 	app := fiber.New()
-	
+
 	app.Use(logger.New())
 	app.Use(requestid.New())
 
 	app.Get("/", dices)
+
+	user := app.Group("/user")
+	user.Get("/", getUser)
+
 	err := app.Listen(":8080")
 	if err != nil {
 		log.Fatal(err)
